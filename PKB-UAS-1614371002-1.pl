b87893ac8648217b42f0daaa1248cc8d6e38ed02
Davidnico1996 <45454332+Davidnico1996@users.noreply.github.com>
2019-01-09 19:45:03 +0700

    Add files via upload

---
+++ PKB-UAS-1614371002-1.pl
@@ -0,0 +1,42 @@
+(suwilo)(p)
+(jodi)(p).
+(ferdinan)(p).
+(suwardi)(p).
+(dani)(p).
+(widodo)(p).
+(wisnu)(p).
+(boni)(p).
+(erianto)(p).
+
+(mirna)(w).
+(kandi)(w).
+(widya)(w).
+(cindi)(w).
+(jamila)(w).
+(winarni)(w).
+(kandi)(w).
+
+(suwilo, mirna)(ortu).
+(suwilo, jodi)(ortu).
+(suwilo, ferdinan)(ortu).
+(jodi, kandi)(ortu).
+(jodi, suwardi)(ortu).
+(mirna, dani)(ortu).
+(ferdinan, cindi)(ortu).
+(ferdinan, widodo)(ortu).
+(kandi, wisnu)(ortu).
+(suwardi, widya)(ortu).
+(suwardi, boni)(ortu).
+(cindi, erianto)(ortu).
+(cindi, jamila)(ortu).
+(widodo, wiharni)(ortu).
+
+(Y,X)(anak):-(X,Y)(ortu).
+(X,Y)(anakpria):-(Y,X)(ortu),(X)(pria).
+(X,Y)(anakwanita):-(Y,X)(ortu),(X)(wanita).
+
+(X,Y)(keturunan):-(X,Y)(anak).
+(X,Y)(keturunan):-(,X,Z)(anak),(Z,Y)(keturunan).
+
+(X,Y)(penerus):-(X,Y)(anak),(X)(p).
+(X,Y)(penerus):-(X,Z)(anak),(Z,Y)(penerus),(x)(p)
